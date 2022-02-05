3b8efc655701deff8bf4b7e8ef31afe0b274a026
Drew Tollett <orecros@gmail.com>
2022-02-04 18:55:47 -0600

    in progress

---
+++ Assets/Scripts/Pogo/LevelDescriptor.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "new level", menuName = "ScriptableObjects/LevelDescriptor", order = 1)]
+public class LevelDescriptor : ScriptableObject
+{
+    public int SceneNumber;
+}
