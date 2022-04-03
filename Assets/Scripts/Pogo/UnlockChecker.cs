8afcf363c1f5dc16d6a95c11aec99339d06285e9
Drew Tollett <orecros@gmail.com>
2022-04-02 20:37:49 -0500

    Unlockable Pickups only spawn if not already unlocked

---
+++ Assets/Scripts/Pogo/UnlockChecker.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using WizardUtils.Saving;
+
+namespace Pogo
+{
+    public class UnlockChecker : MonoBehaviour
+    {
+        public SaveValueDescriptor Save;
+
+        public bool CheckOnAwake = true;
+
+        public UnityEvent OnCheckedLocked;
+        public UnityEvent OnCheckedUnlocked;
+
+        public void Start()
+        {
+            if (CheckOnAwake) Check();
+        }
+
+        public bool Check()
+        {
+            bool unlocked = Save.IsUnlocked;
+
+            if (unlocked)
+            {
+                OnCheckedUnlocked?.Invoke();
+            }
+            else
+            {
+                OnCheckedLocked?.Invoke();
+            }
+
+            return unlocked;
+        }
+    }
+}
