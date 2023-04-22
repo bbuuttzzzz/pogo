89e0dd880e9464742e9145c250cd93470d8b5cb1
Drew Tollett <orecros@gmail.com>
2023-04-21 22:48:35 -0500

    Level3 to C4L1 fixed

---
+++ Assets/Scripts/Pogo/Gimmicks/SlowFaller.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Pogo
+{
+    public class SlowFaller : MonoBehaviour
+    {
+        public float MaxFallSpeed;
+        public float Drag = 1;
+
+        Collider cachedPlayerCollider;
+        PlayerController cachedPlayer;
+        public void Slow(Collider other)
+        {
+            if (cachedPlayerCollider != other)
+            {
+                // LAZY AS FUCK!!!! lol
+                PlayerController player = other.GetComponentInParent<PlayerController>();
+                if (player != null)
+                {
+                    cachedPlayer = player;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            float deltaSpeed = MaxFallSpeed - Vector3.Dot(cachedPlayer.Velocity, Vector3.down);
+            if (deltaSpeed > 0) return;
+
+            cachedPlayer.ApplyForce(Vector3.up * Drag * deltaSpeed * deltaSpeed * Time.deltaTime * Time.deltaTime);
+        }
+    }
+}
