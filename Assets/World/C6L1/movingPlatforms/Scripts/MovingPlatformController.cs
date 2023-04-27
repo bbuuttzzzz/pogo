e1cd04d497fd9e3aae7537cdd811478239e446ba
Drew Tollett <orecros@gmail.com>
2023-04-27 00:07:30 -0500

    yaay staggering is working

--- Assets/World/C6L1/movingPlatforms/Scripts/MovingPlatformController.cs
+++ Assets/World/C6L1/movingPlatforms/Scripts/MovingPlatformController.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class MovingPlatformController : MonoBehaviour
 {
+    Animator animator;
     public float CycleOffset;
     private void Awake()
     {
-        GetComponent<Animator>().SetFloat("CycleOffset", CycleOffset);
-        SetStartingPosition();
+        animator = GetComponent<Animator>();
+        animator.SetFloat("CycleOffset", CycleOffset);
     }
 
     [ContextMenu("Set Starting Position")]
     private void SetStartingPosition()
     {
-        var animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
         animator.Play("Cycle", 0, CycleOffset);
         animator.Update(0);
     }
