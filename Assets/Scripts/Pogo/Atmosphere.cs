f523db0e9d5151bdea1872b9551ec47eb2d040ee
Drew Tollett <orecros@gmail.com>
2023-05-04 01:17:11 -0500

    light sources no longer a part of the gamescene yaaay

--- Assets/Scripts/Pogo/Atmosphere.cs
+++ Assets/Scripts/Pogo/Atmosphere.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -15,8 +16,16 @@ public class Atmosphere : MonoBehaviour
     [ColorUsage(false, true)]
     public Color AmbientLightColor = Color.white;
 
+    public Light TargetLight;
+    private float DefaultLightIntensity;
+
     private void Awake()
     {
+        if (TargetLight != null)
+        {
+            DefaultLightIntensity = TargetLight.intensity;
+            TargetLight.intensity = 0;
+        }
         volume = GetComponent<Volume>();
         volume.weight = 0;
         currentWeight = 0;
@@ -89,5 +98,12 @@ public class Atmosphere : MonoBehaviour
     {
         currentWeight = t;
         volume.weight = t;
+#if UNITY_EDITOR
+        if (UnityEditor.EditorApplication.isPlaying && TargetLight != null)
+        {
+            TargetLight.intensity = DefaultLightIntensity * t;
+        }
+#endif
+
     }
 }
