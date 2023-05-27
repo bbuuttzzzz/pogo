2e731a7f6f82a38428dd5185319d7db77c2eb06d
Drew Tollett <orecros@gmail.com>
2023-05-27 14:47:45 -0500

    a little work on the FlightRing stuff

---
+++ Assets/Scripts/Player/Abilities/PlayerAbility.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+
+namespace Pogo.Abilities
+{
+    public abstract class PlayerAbility : MonoBehaviour
+    {
+        protected PlayerController Target;
+
+        public enum CleanseTypes
+        {
+            CleanseOnDeath,
+            CleanseOnTouch
+        }
+        public CleanseTypes CleanseType;
+
+        public void Apply(PlayerController target)
+        {
+            Target = target;
+            Target.OnTouch.AddListener(Target_OnTouch);
+            Target.OnSpawn.AddListener(Target_OnSpawn);
+            OnApply();
+        }
+
+        private void Target_OnSpawn()
+        {
+            Cleanse();
+        }
+
+        private void Target_OnTouch()
+        {
+            if (CleanseType == CleanseTypes.CleanseOnTouch)
+            {
+                Cleanse();
+            }
+        }
+
+        public void Cleanse()
+        {
+            OnCleanse();
+            Target.OnTouch.RemoveListener(Target_OnTouch);
+            Target = null;
+            Destroy(gameObject);
+        }
+
+        protected abstract void OnApply();
+        protected abstract void OnCleanse();
+    }
+}
