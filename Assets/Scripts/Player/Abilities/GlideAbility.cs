009f057888100abd860f94708b2331257ca0b257
Drew Tollett <orecros@gmail.com>
2023-05-27 15:34:40 -0500

    fix compile error with GlideAbility

--- Assets/Scripts/Player/Abilities/GlideAbility.cs
+++ Assets/Scripts/Player/Abilities/GlideAbility.cs
@@ -5,5 +5,14 @@ namespace Pogo.Abilities
 {
     public class GlideAbility : PlayerAbility
     {
+        protected override void OnApply()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnCleanse()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
