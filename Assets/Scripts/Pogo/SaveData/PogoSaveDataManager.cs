bcf7b6d868099c3a4d7902673f5f410f588749dd
Drew Tollett <orecros@gmail.com>
2023-08-19 21:09:35 -0500

    Added steamworks.NET + some saving stuff

---
+++ Assets/Scripts/Pogo/SaveData/PogoSaveDataManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Pogo.Saving
+{
+    public class PogoSaveDataManager : MonoBehaviour
+    {
+        private SaveSlotDataTracker CurrentTracker;
+
+        public bool TrackerLoaded => CurrentTracker != null;
+
+        public void LoadSlot(int slotId)
+        {
+            if (TrackerLoaded) throw new InvalidOperationException("Tried to load a save slot while one is already loaded");
+        }
+
+        public void SaveCurrentSlot(bool closeAfterSaving = false)
+        {
+
+        }
+    }
+}
