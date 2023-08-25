06ffe2c6e68e9bc76f5e29979bb0b75530e4f48c
Drew Tollett <orecros@gmail.com>
2023-08-24 22:53:48 -0500

    new skulls

--- Assets/Scripts/Pogo/SaveData/SaveSlotPreviewData.cs
+++ Assets/Scripts/Pogo/SaveData/SaveSlotPreviewData.cs
@@ -5,8 +5,10 @@ namespace Pogo.Saving
     [System.Serializable]
     public struct SaveSlotPreviewData
     {
+        public string Name;
         public decimal Percentage;
         public decimal TotalTime;
         public int TotalDeaths;
+        public int LastUnlockedChapter;
     }
 }
