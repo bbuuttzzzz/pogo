504223cf8f9dae6ba43b085b79c186a1f8e4c077
Drew Tollett <orecros@gmail.com>
2023-09-30 15:26:18 -0500

    started build scripts

---
+++ Assets/Scripts/Builder.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Linq;
+using UnityEditor;
+
+public static class Builder
+{
+    public static void BuildAll(string root)
+    {
+        BuildWin64()
+    }
+
+    public static void BuildWin64(string path) => Build(path, BuildTarget.StandaloneWindows64);
+
+    public static void BuildLinux64(string path) => Build(path, BuildTarget.StandaloneLinux64);
+
+    public static void Build(string path, BuildTarget target)
+    {
+        var options = new BuildPlayerOptions
+        {
+            scenes = GetScenes(),
+            target = target,
+            locationPathName = path,
+        };
+
+        BuildPipeline.BuildPlayer(options);
+    }
+
+    private static string[] GetScenes()
+    {
+        return EditorBuildSettings.scenes
+            .Select(s => s.path)
+            .ToArray();
+    }
+}
