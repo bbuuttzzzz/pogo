f34ee66c6feaafcd4f9d824c9f0128c2406d1e83
Drew Tollett <orecros@gmail.com>
2023-09-30 12:06:20 -0500

    Scripts / Patches into version control

---
+++ Scripts/set-version.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+YLW='\033[1;33m'
+RED='\033[1;31m'
+GRN='\033[0;32m'
+NC='\033[0m' # No Color
+
+FILE_NAME='ProjectSettings/ProjectSettings.asset'
+VERSION_MATCH='bundleVersion: .*'
+
+
+display_usage () {
+    echo "Usage: set-version.sh [OPTION]... <Version>"
+}
+#parse positional args
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -*|--*)
+            echo "Unknown option $1"
+	        display_usage
+            exit 1
+            ;;
+        *)
+            POSITIONAL_ARGS+=("$1") # save positional arg
+            shift # past argument
+            ;;
+    esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+set -e
+set -x
+
+NEW_VERSION_LINE="bundleVersion: "$1
+VERSION_REPLACE_PATTERN="s/$VERSION_MATCH/$NEW_VERSION_LINE/g"
+
+sed -i "$VERSION_REPLACE_PATTERN" $FILE_NAME
