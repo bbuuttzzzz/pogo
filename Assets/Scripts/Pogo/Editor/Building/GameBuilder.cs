d95b55c5e803f4e8269032bb9d2f798e06cca494
Drew Tollett <orecros@gmail.com>
2023-10-01 18:22:54 -0500

    some building script stuff

---
+++ Assets/Scripts/Pogo/Editor/Building/GameBuilder.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace Pogo.Building
+{
+    public static class GameBuilder
+    {
+        public struct PogoBuildTarget
+        {
+            public string BuildName;
+            public BuildTarget Target;
+            public string Extension;
+
+            public PogoBuildTarget(string buildName, BuildTarget target, string extension)
+            {
+                BuildName = buildName;
+                Target = target;
+                Extension = extension;
+            }
+        }
+
+        public struct PogoBuildResult
+        {
+            public PogoBuildTarget Target;
+            public BuildResult Result;
+
+            public PogoBuildResult(PogoBuildTarget target, BuildReport report)
+            {
+                Target = target;
+                Result = report.summary.result;
+            }
+        }
+
+        public class FileLogger : IDisposable
+        {
+            string logPath;
+            StreamWriter writer;
+
+            public FileLogger(string logPath)
+            {
+                this.logPath = logPath;
+                writer = File.AppendText(logPath);
+            }
+
+            public void Dispose()
+            {
+                writer.Dispose();
+            }
+
+            public void WriteLine(string message)
+            {
+                Debug.Log(message);
+                writer.WriteLine(message);
+            }
+        }
+
+        public static void BuildAll()
+        {
+            var parser = new CommandLineArgsParser();
+            if (!parser.TryGetArg("-pathRoot", out string pathRoot))
+            {
+                Debug.LogError("Missing REQUIRED argument -pathRoot <value>");
+                return;
+            }
+            if (!parser.TryGetArg("-shortLogs", out string myLogPath))
+            {
+                myLogPath = $"{pathRoot}{Path.DirectorySeparatorChar}Logs.txt";
+            }
+
+            using FileLogger logger = new FileLogger(myLogPath);
+            BuildShared(pathRoot, logger);
+        }
+
+        [MenuItem("Build All")]
+        public static void BuildAllFromEditor()
+        {
+            string pathRoot = $"{Application.dataPath}{Path.PathSeparator}Build";
+            using FileLogger logger = new FileLogger($"{pathRoot}{Path.DirectorySeparatorChar}Logs.txt");
+
+            BuildShared(pathRoot, logger);
+        }
+
+        private static void BuildShared(string pathRoot, FileLogger logger)
+        {
+            logger.WriteLine("Starting Build");
+
+            PogoBuildTarget[] pogoTargets = new PogoBuildTarget[]
+            {
+            new PogoBuildTarget("win32", BuildTarget.StandaloneWindows, ".exe"),
+            new PogoBuildTarget("win64", BuildTarget.StandaloneWindows64, ".exe"),
+            new PogoBuildTarget("linux64", BuildTarget.StandaloneLinux64, "x86.64"),
+            };
+
+            List<PogoBuildResult> Results = new List<PogoBuildResult>();
+
+            foreach (var pogoTarget in pogoTargets)
+            {
+                BuildReport report = Build(
+                    $"{pathRoot}{Path.DirectorySeparatorChar}{pogoTarget.BuildName}{Path.DirectorySeparatorChar}pogo{Path.DirectorySeparatorChar}pogo{pogoTarget.Extension}",
+                    pogoTarget.Target,
+                    logger.WriteLine);
+                Results.Add(new PogoBuildResult(pogoTarget, report));
+            }
+
+            foreach (var result in Results)
+            {
+                logger.WriteLine("Final Results: ");
+
+                logger.WriteLine($"\t{result.Target.BuildName}: {result.Result}");
+            }
+        }
+
+        public static BuildReport Build(string path, BuildTarget target, Action<string> logAction)
+        {
+            logAction?.Invoke($"[{target}] Building to {path}");
+            var options = new BuildPlayerOptions
+            {
+                scenes = GetScenes(),
+                target = target,
+                locationPathName = path,
+            };
+
+            var result = BuildPipeline.BuildPlayer(options);
+
+            string resultText = result.summary.result == UnityEditor.Build.Reporting.BuildResult.Succeeded
+                ? "SUCCESS"
+                : "FAILURE";
+
+            logAction?.Invoke($"[{target}] Build {resultText}");
+            return result;
+        }
+
+        private static string[] GetScenes()
+        {
+            return EditorBuildSettings.scenes
+                .Select(s => s.path)
+                .ToArray();
+        }
+    }
+}
--- Scripts/build-and-pack.sh
+++ Scripts/build-and-pack.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-
 UNITY_PATH="/Q/Programs/Unity/2021.3.30f1/Editor/Unity.exe"
 PROJECT_PATH="$(pwd)"
-BUILD_PATH_WIN="$PROJECT_PATH/Build/pogo"
-BUILD_PATH_LINUX="$PROJECT_PATH/Build_Linux/pogo"
-BUILD_NAME_LINUX="horde.x86_64"
-
-LOG_PATH_WIN="$PROJECT_PATH/Build/build-cli-logs.txt"
-LOG_PATH_LINUX="$PROJECT_PATH/Build_Linux/build-cli-logs.txt"
+LOG_FILE=$PROJECT_PATH/Build/CoolLogs.txt
 
 set -e
 
-echo "Building Windows to $BUILD_PATH_WIN"
-$UNITY_PATH -batchmode -buildTarget Win64 -configuration Release -buildLocation "$BUILD_PATH" - logfile -
+touch $LOG_FILE
+
+echo "Launching Unity..."
+$UNITY_PATH -quit -batchmode -executeMethod "Pogo.Building.GameBuilder.BuildAll" -pathRoot "$PROJECT_PATH/Build" -shortLogs $LOG_FILE -logfile $PROJECT_PATH/Build/full-build-logs.txt &
+mypid=$!
+tail -f -n +0 $LOG_FILE &
+wait $mypid
+kill $mypid
