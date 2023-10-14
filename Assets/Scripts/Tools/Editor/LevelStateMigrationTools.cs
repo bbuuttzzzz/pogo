02e4300f814d9bc48740c803a8a96a80d41cb11f
Drew Tollett <orecros@gmail.com>
2023-10-14 17:06:37 -0500

    DeveloperChallenge migration

--- Assets/Scripts/Tools/Editor/LevelStateMigrationTools.cs
+++ Assets/Scripts/Tools/Editor/LevelStateMigrationTools.cs
@@ -10,6 +10,43 @@ namespace Pogo.Tools
 {
     public static class LevelStateMigrationTools
     {
+        [MenuItem("Pogo/Migration Tools/Challenges")]
+        public static void MigrateChallenges()
+        {
+
+            var pathPairs = AssetDatabase.FindAssets($"t:{nameof(DeveloperChallenge)}")
+                .Select(id =>
+                {
+                    var path = AssetDatabase.GUIDToAssetPath(id);
+                    return new
+                    {
+                        Path = path,
+                        Asset = AssetDatabase.LoadAssetAtPath<DeveloperChallenge>(path)
+                    };
+                });
+
+            foreach (var pathPair in pathPairs)
+            {
+                if (pathPair.Asset.Challenge.LevelState.Level == pathPair.Asset.Challenge.Level)
+                {
+                    Debug.Log($"Skipping already migrated developerChallenge @ {pathPair.Path}");
+                }
+                else
+                {
+                    Debug.Log($"Migrating developerChallenge @ {pathPair.Path}");
+                    pathPair.Asset.Challenge.LevelState = new Levels.LevelState()
+                    {
+                        Level = pathPair.Asset.Challenge.Level,
+                        StateId = 0
+                    };
+
+                    EditorUtility.SetDirty(pathPair.Asset);
+                }
+            }
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
 
         private static string[] GetAllScenePaths()
         {
