47f3906a10ecb3097925602b58196ce59d462315
Drew Tollett <orecros@gmail.com>
2023-10-17 21:05:22 -0500

    fixed Slow Faller D:

--- Assets/Scripts/Pogo/Gimmicks/SlowFaller.cs
+++ Assets/Scripts/Pogo/Gimmicks/SlowFaller.cs
@@ -9,26 +9,58 @@ namespace Pogo
 
         Collider cachedPlayerCollider;
         PlayerController cachedPlayer;
-        public void Slow(Collider other)
+
+        private ContinuousForce Force;
+
+        private void Awake()
         {
-            if (cachedPlayerCollider != other)
+            Force = new ContinuousForce(this);
+        }
+
+        public void Apply(Collider other)
+        {
+            PlayerController player = other.GetComponentInParent<PlayerController>();
+            if (player != null)
             {
-                // LAZY AS FUCK!!!! lol
-                PlayerController player = other.GetComponentInParent<PlayerController>();
-                if (player != null)
-                {
-                    cachedPlayer = player;
-                }
-                else
-                {
-                    return;
-                }
+                player.AddContinuousForce(Force);
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        public void Remove(Collider other)
+        {
+            PlayerController player = other.GetComponentInParent<PlayerController>();
+            if (player != null)
+            {
+                player.RemoveContinuousForce(Force);
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        public class ContinuousForce : IPlayerContinuousForce
+        {
+            private SlowFaller parent;
+
+            public ContinuousForce(SlowFaller parent)
+            {
+                this.parent = parent;
             }
 
-            float deltaSpeed = MaxFallSpeed - Vector3.Dot(cachedPlayer.Velocity, Vector3.down);
-            if (deltaSpeed > 0) return;
+            public bool IsValid() => parent != null;
 
-            cachedPlayer.ApplyForce(Vector3.up * Drag * deltaSpeed * deltaSpeed * Time.deltaTime * Time.deltaTime);
+            public Vector3 GetForce(PlayerController player, float deltaTime)
+            {
+                float deltaSpeed = parent.MaxFallSpeed - Vector3.Dot(player.Velocity, Vector3.down);
+                if (deltaSpeed > 0) return Vector3.zero;
+
+                return Vector3.down * parent.Drag * deltaSpeed * deltaTime;
+            }
         }
     }
 }
