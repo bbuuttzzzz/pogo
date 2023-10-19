ab10d7e6e4db2a616aeb28ef8137cf992e13be82
Drew Tollett <orecros@gmail.com>
2023-10-18 20:28:41 -0500

    more glide particle stuff (better this time)

--- Assets/Entities/Abilities/Flight/Scripts/GlideParticleSystemController.cs
+++ Assets/Entities/Abilities/Flight/Scripts/GlideParticleSystemController.cs
@@ -8,48 +8,45 @@ public class GlideParticleSystemController : MonoBehaviour
     // Start is called before the first frame update
     public ParticleSystem Target;
 
-    private float EmissionRate;
-    private PlayerController Parent;
+    private Transform lastOrientationTransform;
+    private Vector3 lastOrientationPosition;
+    private Quaternion lastOrientationRotation;
+    public AnimationCurve SpaceChangeCurve;
+
+    ParticleSystem.Particle[] particles;
+    int currentParticleCount = 0;
 
     public void Awake()
     {
-        UpdateSettingsFromTarget();
+        lastOrientationTransform = new GameObject("lastOrientation").transform;
+        lastOrientationTransform.parent = transform;
+
+        particles = new ParticleSystem.Particle[Target.main.maxParticles];
     }
 
-    [ContextMenu("Update Settings from Target")]
-    private void UpdateSettingsFromTarget()
-    {
-        EmissionRate = Target.emission.rateOverTime.constant;
-    }
-
-    float emissionBudget = 0;
-
-    public void SetParent(PlayerController parent)
-    {
-        Parent = parent;
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Parent == null)
+        currentParticleCount = Target.GetParticles(particles);
+        lastOrientationTransform.position = lastOrientationPosition;
+        lastOrientationTransform.rotation = lastOrientationRotation;
+
+        for(int n = 0; n < currentParticleCount; n++)
         {
-            return;
+            Vector3 previousLocalPosition = lastOrientationTransform.InverseTransformPoint(particles[n].position);
+            Vector3 nextRelativeWorldPosition = transform.TransformPoint(previousLocalPosition);
+            Vector3 deltaWorldPosition = nextRelativeWorldPosition - particles[n].position;
+
+            float tRaw = 1 - particles[n].remainingLifetime / particles[n].startLifetime;
+
+            Vector3 finalPosition = Vector3.Lerp(particles[n].position, particles[n].position + deltaWorldPosition, SpaceChangeCurve.Evaluate(tRaw));
+            particles[n].position = finalPosition;
+
         }
 
-        emissionBudget += Time.deltaTime * EmissionRate;
-
-        while (emissionBudget > 0)
-        {
-            Debug.Log("Emit!");
-            emissionBudget--;
-            EmitParticles(1);
-        }
-    }
-
-    private void EmitParticles(int count)
-    {
-        ParticleSystem.EmitParams p = new ParticleSystem.EmitParams();
-        Target.Emit(p, count);
+        Target.SetParticles(particles);
+        lastOrientationPosition = transform.position;
+        lastOrientationRotation = transform.rotation;
     }
 }
