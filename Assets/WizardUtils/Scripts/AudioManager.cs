96777a99dceac5829970b18508ca57c05caa626e
Drew Tollett <orecros@gmail.com>
2023-10-20 20:35:36 -0500

    WizardUtils Alt Tab mute functionality

--- Assets/WizardUtils/Scripts/AudioManager.cs
+++ Assets/WizardUtils/Scripts/AudioManager.cs
@@ -1,37 +1,75 @@
-﻿using UnityEngine;
+﻿using Pogo;
+using System;
+using System.Linq;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
 using UnityEngine.Audio;
+using WizardUtils.Audio;
 
 namespace WizardUtils
 {
     [RequireComponent(typeof(GameManager))]
     public class AudioManager : MonoBehaviour
     {
+        private bool shouldMuteOnLoseFocus;
+        private bool applicationHasFocus;
+        private bool audioManagerSetUp = false;
+
         public AudioMixer mixer;
-        public AudioChannelSettingPair[] Channels;
+        public AudioChannelSettingPair[] AdditionalChannels;
+        private static AudioChannelSettingPair[] BaseChannels => new AudioChannelSettingPair[]
+        {
+            new AudioChannelSettingPair("MasterVolume", GameManager.KEY_VOLUME_MASTER),
+            new AudioChannelSettingPair("EffectVolume", GameManager.KEY_VOLUME_EFFECTS),
+            new AudioChannelSettingPair("AmbienceVolume", GameManager.KEY_VOLUME_AMBIENCE),
+            new AudioChannelSettingPair("MusicVolume", GameManager.KEY_VOLUME_MUSIC)
+        };
+
+        private AudioChannelController[] Controllers;
 
         private void Start()
         {
             GameManager instance = GetComponent<GameManager>();
-            foreach(var channel in Channels)
+            Controllers = new AudioChannelController[BaseChannels.Length];
+
+            var allChannels = GenerateAllChannels();
+            Controllers = new AudioChannelController[allChannels.Length];
+            for (int n = 0; n < allChannels.Length; n++)
             {
-                var setting = instance.FindGameSetting(channel.GameSettingKey);
-                mixer.SetFloat(channel.MixerParamName, linearToLog(setting.Value));
-                setting.OnChanged += (sender, e) => {
-                    mixer.SetFloat(channel.MixerParamName, linearToLog(e.FinalValue));
-                };
+                var setting = instance.FindGameSetting(allChannels[n].GameSettingKey);
+                Controllers[n] = new AudioChannelController(mixer, setting, allChannels[n].MixerParamName);
             }
+
+            GameSettingFloat altTabMuteSetting = GameManager.GameInstance.FindGameSetting(GameManager.SETTINGKEY_MUTE_ON_ALT_TAB);
+            altTabMuteSetting.OnChanged += AltTabMuteSetting_OnChanged;
+            shouldMuteOnLoseFocus = altTabMuteSetting.Value == 1;
+            audioManagerSetUp = true;
         }
 
-        private float linearToLog(float value)
+        private void OnApplicationFocus(bool focus)
         {
-            return Mathf.Log10(value / 100f) * 20;
+            if (!audioManagerSetUp) return;
+            applicationHasFocus = focus;
+            RecalculateShouldMute();
         }
-    }
 
-    [System.Serializable]
-    public struct AudioChannelSettingPair
-    {
-        public string MixerParamName;
-        public string GameSettingKey;
+
+        private void AltTabMuteSetting_OnChanged(object sender, GameSettingChangedEventArgs e)
+        {
+            shouldMuteOnLoseFocus = e.FinalValue == 1;
+            RecalculateShouldMute();
+        }
+
+        private void RecalculateShouldMute()
+        {
+            SetGameMuted(shouldMuteOnLoseFocus && !applicationHasFocus);
+        }
+
+        public void SetGameMuted(bool muted)
+        {
+            Controllers[0].Muted = muted;
+        }
+
+        private AudioChannelSettingPair[] GenerateAllChannels() => BaseChannels.Concat(AdditionalChannels).ToArray();
     }
 }
