69e520a4b76327be845d8e148dbb6135ea577d9d
Drew Tollett <orecros@gmail.com>
2023-10-21 17:51:18 -0500

    StartChallengeMenuController should properly call show/hide events

--- Assets/Project/UI/StartChallengeMenu/StartChallengeMenuController.cs
+++ Assets/Project/UI/StartChallengeMenu/StartChallengeMenuController.cs
@@ -3,10 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using WizardUtils;
+
+[RequireComponent(typeof(ToggleableUIElement))]
 
 public class StartChallengeMenuController : MonoBehaviour
 {
     public UnityEvent OnCloseMenu;
+
+    private void Awake()
+    {
+        var selfToggleable = GetComponent<ToggleableUIElement>();
+        selfToggleable.OnOpen.AddListener(ShowChallengePreview);
+        selfToggleable.OnClose.AddListener(HideChallengePreview);
+    }
 
     public void CloseMenu() => OnCloseMenu?.Invoke();
     public void PlaceBalloon()
@@ -15,6 +25,9 @@ public class StartChallengeMenuController : MonoBehaviour
         PogoGameManager.PogoInstance.ChallengeBuilder.CalculateNewChallenge();
         PogoGameManager.PogoInstance.ChallengeBuilder.LoadChallenge();
     }
+
+    private void ShowChallengePreview() => SetShowChallengePreview(true);
+    private void HideChallengePreview() => SetShowChallengePreview(false);
 
     public void SetShowChallengePreview(bool state)
     {
