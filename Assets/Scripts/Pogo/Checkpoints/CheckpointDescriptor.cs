123c886b248b373faf1ead917ef049378f925808
Drew Tollett <orecros@gmail.com>
2023-10-29 14:46:07 -0500

    Revert "migrate tool for CheckpointDescriptor LevelState->MainLevelState"
    This reverts commit 563b565e223f45421ee009fbc607f337c5437fcb.

--- Assets/Scripts/Pogo/Checkpoints/CheckpointDescriptor.cs
+++ Assets/Scripts/Pogo/Checkpoints/CheckpointDescriptor.cs
@@ -11,7 +11,6 @@ namespace Pogo.Checkpoints
     [CreateAssetMenu(fileName = "ck_", menuName = "Pogo/CheckpointDescriptor")]
     public class CheckpointDescriptor : ScriptableObject
     {
-        public LevelState LevelState;
         public LevelState MainLevelState;
         public LevelState[] AdditionalLevelStates;
         public int Par;
