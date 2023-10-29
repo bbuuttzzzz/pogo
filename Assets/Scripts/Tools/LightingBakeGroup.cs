415cefc01bb90be4c2e1398bcda09ea83e730313
Drew Tollett <orecros@gmail.com>
2023-10-28 22:10:58 -0500

    lighting baking stuffs :D

---
+++ Assets/Scripts/Tools/LightingBakeGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace Pogo.Tools
+{
+    [CreateAssetMenu(fileName ="bakeGroup_", menuName = "Pogo/Lighting Bake Group")]
+    public class LightingBakeGroup : ScriptableObject
+    {
+        public SceneAsset MainScene;
+        public SceneAsset[] OtherScenes;
+    }
+}
