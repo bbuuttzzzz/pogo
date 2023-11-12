71e40fdbc2e18557e9bfe2cccfccc9dc8fb4de88
Drew Tollett <orecros@gmail.com>
2023-11-12 13:23:58 -0600

    SeralizedObjectHelpers

---
+++ Assets/WizardUtils/Scripts/Editor/SerializedObjectHelpers/SerializedObjectUpdater.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+
+namespace WizardUtils.SerializedObjectHelpers
+{
+    public class SerializedObjectUpdater
+    {
+        private List<SerializedPropertyChangeHandler> Handlers;
+
+        public SerializedObjectUpdater()
+        {
+            Handlers = new List<SerializedPropertyChangeHandler>();
+        }
+
+        public void Add<T>(Func<T> get, Action<SerializedPropertyChangedArgs<T>> onChangedCallback, EqualityComparer<T> equalityComparer = null)
+        {
+            SerializedPropertyChangeHandler<T> item = new SerializedPropertyChangeHandler<T>(get, onChangedCallback, equalityComparer);
+            Handlers.Add(item);
+        }
+
+        public void Check()
+        {
+            foreach(var handler in Handlers)
+            {
+                handler.Check();
+            }
+        }
+    }
+}
---
+++ Assets/WizardUtils/Scripts/Editor/SerializedObjectHelpers/SerializedPropertyChangeHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizardUtils.SerializedObjectHelpers
+{
+    public abstract class SerializedPropertyChangeHandler
+    {
+        public abstract void Store();
+        
+        public abstract void Check();
+    }
+
+    public class SerializedPropertyChangeHandler<T> : SerializedPropertyChangeHandler
+    {
+        private EqualityComparer<T> EqualityComparer;
+        private Action<SerializedPropertyChangedArgs<T>> OnChangedCallback;
+        private Func<T> Get;
+
+        private T cachedValue;
+
+        public SerializedPropertyChangeHandler(Func<T> get, Action<SerializedPropertyChangedArgs<T>> onChangedCallback, EqualityComparer<T> equalityComparer = null)
+        {
+            OnChangedCallback = onChangedCallback;
+            EqualityComparer = equalityComparer ?? EqualityComparer<T>.Default;
+            Get = get;
+            Store();
+        }
+
+        public override void Store()
+        {
+            cachedValue = Get();
+        }
+
+        public override void Check()
+        {
+            T newValue = Get();
+            if (!EqualityComparer.Equals(cachedValue, newValue))
+            {
+                OnChangedCallback.Invoke(new SerializedPropertyChangedArgs<T>(cachedValue, newValue));
+            }
+        }
+    }
+}
