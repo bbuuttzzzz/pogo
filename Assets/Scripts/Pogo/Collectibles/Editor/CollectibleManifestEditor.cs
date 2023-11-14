c4d725b43a446b8513db7b847e3f71623115bf1f
Drew Tollett <orecros@gmail.com>
2023-11-13 21:13:46 -0600

    collectible manifest shows counts of different collectible types

---
+++ Assets/Scripts/Pogo/Collectibles/Editor/CollectibleManifestEditor.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Pogo.Collectibles
+{
+    [CustomEditor(typeof(CollectibleManifest))]
+    public class CollectibleManifestEditor : Editor
+    {
+        CollectibleManifest self;
+
+        public override VisualElement CreateInspectorGUI()
+        {
+            self = target as CollectibleManifest;
+            return base.CreateInspectorGUI();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.LabelField("Collectible Type Counts");
+            using (new EditorGUI.IndentLevelScope())
+            {
+                foreach (var group in self.Collectibles.GroupBy(c => c.CollectibleType).OrderBy(x => x.Key))
+                {
+                    using (new EditorGUI.DisabledScope(true))
+                    {
+                        EditorGUILayout.IntField(group.Count(), group.Key.ToString());
+                    }
+                }
+            }
+        }
+
+    }
+}
