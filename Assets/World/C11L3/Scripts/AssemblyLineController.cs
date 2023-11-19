ca18b46353824e8741c5a262a0f14b1a6b608405
Drew Tollett <orecros@gmail.com>
2023-11-19 17:08:08 -0600

    C11L3 stuff

---
+++ Assets/World/C11L3/Scripts/AssemblyLineController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pogo.AssemblyLines
+{
+    public class AssemblyLineController : MonoBehaviour
+    {
+        public AnimationClip TargetAnimationClip;
+        public GameObject Prefab;
+        public float DelaySeconds;
+
+        public void Repopulate()
+        {
+
+        }
+    }
+}
