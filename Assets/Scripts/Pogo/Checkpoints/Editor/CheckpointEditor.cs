59c05853a1878455550e479d01a92e02b7cc7eec
Drew Tollett <orecros@gmail.com>
2024-02-05 21:55:59 -0600

    fix checkpoint editors

--- Assets/Scripts/Pogo/Checkpoints/Editor/CheckpointEditor.cs
+++ Assets/Scripts/Pogo/Checkpoints/Editor/CheckpointEditor.cs
@@ -15,9 +15,16 @@ namespace Pogo
     {
         protected Checkpoint self;
 
+        SerializedProperty m_SkipBehavior;
+        SerializedProperty m_OnSkip;
+        SerializedProperty m_SkipTarget;
+
         public override VisualElement CreateInspectorGUI()
         {
             self = target as Checkpoint;
+            m_SkipBehavior = serializedObject.FindProperty(nameof(self.SkipBehavior));
+            m_OnSkip = serializedObject.FindProperty(nameof(self.OnSkip));
+            m_SkipTarget = serializedObject.FindProperty(nameof(self.SkipTarget));
 
             return base.CreateInspectorGUI();
         }
@@ -65,9 +72,69 @@ namespace Pogo
 
         protected abstract bool TryValidate(out string failReason);
 
-        protected abstract void DrawSkipProperties();
+        protected void DrawSkipProperties()
+        {
+            DrawOverrideCheckpointProperty();
+            EditorGUILayout.PropertyField(m_SkipBehavior);
+            if (self.SkipBehavior != SkipBehaviors.LevelChange)
+            {
+                EditorGUILayout.PropertyField(m_OnSkip);
+                EditorGUILayout.PropertyField(m_SkipTarget);
+                DrawSkipSelectDropdown();
+            }
+        }
 
+        protected abstract void DrawOverrideCheckpointProperty();
+        private void DrawSkipSelectDropdown()
+        {
+            if (EditorGUILayout.DropdownButton(new GUIContent("Target SkipPoint..."), FocusType.Passive))
+            {
+                var spawnPoints = GameObject.FindGameObjectsWithTag("SkipPoint");
 
+                var itempaths = new List<(GameObject item, string path)>();
+                foreach (var spawnPoint in spawnPoints)
+                {
+                    string path = AnimationUtility.CalculateTransformPath(spawnPoint.transform, null);
+                    path = path.Replace("/", " -> ");
+
+                    itempaths.Add((spawnPoint, path));
+                }
+
+                GenericMenu menu = new GenericMenu();
+                foreach (var itempath in itempaths.OrderBy(ip => ip.path))
+                {
+                    if (itempath.item.scene == self.gameObject.scene)
+                    {
+                        menu.AddItem(new GUIContent(itempath.path), false, () =>
+                        {
+                            Undo.RecordObject(self, "Select SkipTarget");
+                            self.SkipTarget = itempath.item.transform;
+                            PrefabUtility.RecordPrefabInstancePropertyModifications(self);
+                        });
+                    }
+                    else
+                    {
+                        menu.AddItem(new GUIContent($"[CLONE] {itempath.path}"), false, () =>
+                        {
+                            var clone = Instantiate(itempath.item, self.transform);
+                            clone.transform.position = itempath.item.transform.position;
+                            clone.transform.rotation = itempath.item.transform.rotation;
+                            clone.name = "EXIT SkipPoint Clone";
+
+                            using (new UndoScope("Clone & Select SkipTarget"))
+                            {
+                                Undo.RegisterCreatedObjectUndo(clone, "CLONE Select SkipTarget");
+                                Undo.RecordObject(self, "Select SkipTarget");
+                                self.SkipTarget = clone.transform;
+                                PrefabUtility.RecordPrefabInstancePropertyModifications(self);
+                            }
+                        });
+                    }
+
+                }
+                menu.DropDown(new Rect(Event.current.mousePosition.x, Event.current.mousePosition.y, 0f, 0f));
+            }
+        }
 
         protected virtual void UpdateCanSkip(bool newValue)
         {
--- Assets/Scripts/Pogo/Checkpoints/Editor/ExplicitCheckpointEditor.cs
+++ Assets/Scripts/Pogo/Checkpoints/Editor/ExplicitCheckpointEditor.cs
@@ -17,19 +17,6 @@ namespace Pogo
     {
         new protected ExplicitCheckpoint self => base.self as ExplicitCheckpoint;
 
-        SerializedProperty m_SkipBehavior;
-        SerializedProperty m_OnSkip;
-        SerializedProperty m_SkipTarget;
-
-        public override VisualElement CreateInspectorGUI()
-        {
-            var element = base.CreateInspectorGUI();
-            m_SkipBehavior = serializedObject.FindProperty(nameof(self.SkipBehavior));
-            m_OnSkip = serializedObject.FindProperty(nameof(self.OnSkip));
-            m_SkipTarget = serializedObject.FindProperty(nameof(self.SkipTarget));
-            return element;
-        }
-
         protected override bool TryValidate(out string failReason)
         {
             if (self.Descriptor == null)
@@ -42,18 +29,7 @@ namespace Pogo
             return true;
         }
 
-        protected override void DrawSkipProperties()
-        {
-            DrawOverrideCheckpointProperty();
-            EditorGUILayout.PropertyField(m_SkipBehavior);
-            if (self.SkipBehavior != SkipBehaviors.LevelChange)
-            {
-                EditorGUILayout.PropertyField(m_OnSkip);
-                EditorGUILayout.PropertyField(m_SkipTarget);
-                DrawSkipSelectDropdown();
-            }
-        }
-        private void DrawOverrideCheckpointProperty()
+        protected override void DrawOverrideCheckpointProperty()
         {
             if (self.Descriptor.OverrideSkipToCheckpoint == null
                 && self.Descriptor.CheckpointId.CheckpointType == CheckpointTypes.SidePath)
@@ -73,58 +49,6 @@ namespace Pogo
             self.Descriptor.OverrideSkipToCheckpoint = result;
             EditorUtility.SetDirty(self.Descriptor);
         }
-
-        private void DrawSkipSelectDropdown()
-        {
-            if (EditorGUILayout.DropdownButton(new GUIContent("Target SkipPoint..."), FocusType.Passive))
-            {
-                var spawnPoints = GameObject.FindGameObjectsWithTag("SkipPoint");
-
-                var itempaths = new List<(GameObject item, string path)>();
-                foreach (var spawnPoint in spawnPoints)
-                {
-                    string path = AnimationUtility.CalculateTransformPath(spawnPoint.transform, null);
-                    path = path.Replace("/", " -> ");
-
-                    itempaths.Add((spawnPoint, path));
-                }
-
-                GenericMenu menu = new GenericMenu();
-                foreach (var itempath in itempaths.OrderBy(ip => ip.path))
-                {
-                    if (itempath.item.scene == self.gameObject.scene)
-                    {
-                        menu.AddItem(new GUIContent(itempath.path), false, () =>
-                        {
-                            Undo.RecordObject(self, "Select SkipTarget");
-                            self.SkipTarget = itempath.item.transform;
-                            PrefabUtility.RecordPrefabInstancePropertyModifications(self);
-                        });
-                    }
-                    else
-                    {
-                        menu.AddItem(new GUIContent($"[CLONE] {itempath.path}"), false, () =>
-                        {
-                            var clone = Instantiate(itempath.item, self.transform);
-                            clone.transform.position = itempath.item.transform.position;
-                            clone.transform.rotation = itempath.item.transform.rotation;
-                            clone.name = "EXIT SkipPoint Clone";
-
-                            using (new UndoScope("Clone & Select SkipTarget"))
-                            {
-                                Undo.RegisterCreatedObjectUndo(clone, "CLONE Select SkipTarget");
-                                Undo.RecordObject(self, "Select SkipTarget");
-                                self.SkipTarget = clone.transform;
-                                PrefabUtility.RecordPrefabInstancePropertyModifications(self);
-                            }
-                        });
-                    }
-
-                }
-                menu.DropDown(new Rect(Event.current.mousePosition.x, Event.current.mousePosition.y, 0f, 0f));
-            }
-        }
-
 
         protected override void UpdateCanSkip(bool newValue)
         {
