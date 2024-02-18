34a1bfa6138bab96a8dfe6755a22904b69610418
Drew Tollett <orecros@gmail.com>
2024-02-18 02:36:42 -0600

    delete BurstDebugFolder :3 fuck you unity~!!!

---
+++ Assets/Scripts/Pogo/Editor/Building/DeleteBurstDebugFolderPostProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace Pogo.Building
+{
+    public static class DeleteBurstDebugFolderPostProcessor
+    {
+        [PostProcessBuild(1)]
+        public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+        {
+            if (target == BuildTarget.WebGL) return;
+            Directory.Delete($"{pathToBuiltProject}{Path.DirectorySeparatorChar}pogo_BurstDebugInformation_DoNotShip", true);
+        }
+    }
+}
