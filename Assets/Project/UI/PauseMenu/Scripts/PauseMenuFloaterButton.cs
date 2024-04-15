d56e33eb1d1da8d9f5ecd865db1acdaafa83e390
Drew Tollett <orecros@gmail.com>
2024-04-14 23:49:29 -0500

    redo pause screen floater buttons to be more programmatic

---
+++ Assets/Project/UI/PauseMenu/Scripts/PauseMenuFloaterButton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Pogo.PauseMenu
+{
+    public class PauseMenuFloaterButton
+    {
+        public Button Button;
+        public Func<DisplayTypes> GetDisplayType;
+        public Action OnActivated;
+
+        public enum DisplayTypes
+        {
+            Enabled,
+            Disabled,
+            Hidden
+        }
+    }
+}
---
+++ Assets/Project/UI/PauseMenu/Scripts/PauseMenuFloaterButtonsTab.cs
@@ -0,0 +1,112 @@
+using Pogo;
+using Pogo.Difficulties;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using WizardUtils;
+
+namespace Pogo.PauseMenu
+{
+    public class PauseMenuFloaterButtonsTab : MonoBehaviour
+    {
+        public Transform ButtonsRoot;
+        public PauseMenuController pauseMenuController;
+        public GameObject SkipButtonPrefab;
+        public GameObject RestartButtonPrefab;
+
+        private PauseMenuFloaterButton[] pauseMenuFloaterButtons;
+
+        private void Start()
+        {
+            pauseMenuFloaterButtons = new[]
+            {
+                MakeSkipButton(), // we use this index in Skip()
+                MakeRestartButton()
+            };
+            GameManager.GameInstance.OnPauseStateChanged += onPauseStateChanged;
+        }
+
+        protected virtual void onPauseStateChanged(object sender, bool nowPaused)
+        {
+            if (nowPaused)
+            {
+                Refresh();
+            }
+        }
+
+
+        private PauseMenuFloaterButton MakeSkipButton()
+        {
+            var button = new PauseMenuFloaterButton()
+            {
+                Button = Instantiate(SkipButtonPrefab, ButtonsRoot).GetComponent<Button>(),
+                GetDisplayType = GetSkipButtonDisplayType
+            };
+
+            button.Button.onClick.AddListener(Skip);
+
+            return button;
+        }
+
+        private PauseMenuFloaterButton.DisplayTypes GetSkipButtonDisplayType()
+        {
+            if (PogoGameManager.PogoInstance.CurrentDifficulty != Difficulty.Assist)
+            {
+                return PauseMenuFloaterButton.DisplayTypes.Hidden;
+            }
+
+            if (PogoGameManager.PogoInstance.CanSkipCheckpoint())
+            {
+                return PauseMenuFloaterButton.DisplayTypes.Enabled;
+            }
+
+            return PauseMenuFloaterButton.DisplayTypes.Disabled;
+        }
+
+        private PauseMenuFloaterButton MakeRestartButton()
+        {
+            var button = new PauseMenuFloaterButton()
+            {
+                Button = Instantiate(RestartButtonPrefab, ButtonsRoot).GetComponent<Button>(),
+                GetDisplayType = GetRestartButtonDisplayType
+            };
+
+            button.Button.onClick.AddListener(Restart);
+
+            return button;
+        }
+
+        private PauseMenuFloaterButton.DisplayTypes GetRestartButtonDisplayType()
+        {
+            return PauseMenuFloaterButton.DisplayTypes.Enabled;
+        }
+
+        private void Restart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Refresh()
+        {
+            foreach(var button in pauseMenuFloaterButtons)
+            {
+                var state = button.GetDisplayType();
+                button.Button.gameObject.SetActive(state != PauseMenuFloaterButton.DisplayTypes.Hidden);
+                button.Button.interactable = state != PauseMenuFloaterButton.DisplayTypes.Disabled;
+            }
+        }
+
+        public void Skip()
+        {
+            if (!PogoGameManager.PogoInstance.TrySkipCheckpoint())
+            {
+                Debug.LogError($"Failed to skip CurrentCheckpoint: {PogoGameManager.PogoInstance.CurrentCheckpoint}");
+                pauseMenuFloaterButtons[0].Button.interactable = false;
+                return;
+            }
+
+            pauseMenuController.Resume();
+        }
+    }
+}
